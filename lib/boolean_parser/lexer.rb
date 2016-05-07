608acb3e0a6c828c4fa405e7169d8f99b0856a74
Chakrit Wichian <service@chakrit.net>
2016-05-07 10:46:00 +0700

    Adds lexer + tests.

---
+++ lib/boolean_parser/lexer.rb
@@ -0,0 +1,124 @@
+module BooleanParser
+  class Lexer
+    def initialize(input)
+      @input  = input
+      @index  = 0
+      @tokens = []
+      @buffer = ''
+    end
+
+    def lex
+      @tokens = []
+      lex_line
+      @tokens
+    end
+
+    def lex_line
+      while !eof?
+        case peek
+        when '"' then lex_quoted_word
+        when ':', '+', '-' then lex_symbol
+        when ' ' then lex_spaces
+        else
+          lex_word
+        end
+      end
+    end
+
+    def lex_symbol
+      case consume
+      when ':' then emit(:colon)
+      when '+' then emit(:plus)
+      when '-' then emit(:minus)
+      else
+        raise 'not a sumbol'
+      end
+    end
+
+    def lex_spaces
+      consume while peek == ' '
+      emit(:space)
+    end
+
+    def lex_word
+      while !eof?
+        case peek
+        when ' ', ':', '+', '-' then return emit(:term)
+        else
+          consume
+        end
+      end
+
+      emit(:term)
+    end
+
+    def lex_quoted_word
+      raise 'not in quoted word' unless discard == '"'
+
+      while !eof?
+        case peek
+        when '\\' then lex_escaped_char
+        when '"'
+          discard
+          break
+        else
+          consume
+        end
+      end
+
+      emit(:term)
+    end
+
+    def lex_escaped_char
+      raise 'not in escaped char' unless discard == '\\'
+      return @buffer += '\\' if eof?
+
+      case peek
+      when 'r'
+        discard
+        @buffer += "\r"
+      when 'n'
+        discard
+        @buffer += "\n"
+      else # '\\' '"' and everything else is treated literally
+        consume
+      end
+    end
+
+    private
+
+    def eof?
+      @index >= @input.length
+    end
+
+    def chars_left
+      @input.length - @index
+    end
+
+    def peek
+      return nil if eof?
+      @input[@index]
+    end
+
+    def consume
+      raise 'consume beyond EOF' if eof?
+
+      @buffer += peek
+      discard
+    end
+
+    def discard
+      @index += 1
+      @input[@index - 1]
+    end
+
+    def emit(type)
+      return nil if @buffer == ''
+
+      token = Token.new(type, @buffer)
+      @tokens << token
+      @buffer = ''
+      token
+    end
+  end
+end
---
+++ test/boolean_parser/lexer_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+module BooleanParser
+  class LexerTest < Minitest::Test
+    TESTS = {
+      'word'          => [:term, 'word'],
+      'space  space'  => [:term, 'space', :space, '  ', :term, 'space'],
+      '::++--'        => [:colon, ':', :colon, ':', :plus, '+', :plus, '+', :minus, '-', :minus, '-'],
+      '"quote"'       => [:term, 'quote'],
+      '"unended'      => [:term, 'unended'],
+      '"space quote"' => [:term, 'space quote'],
+      '":sym+quote-"' => [:term, ':sym+quote-'],
+      '"\r\n\n\r\""'  => [:term, "\r\n\n\r\""],
+      'a "b b"'       => [:term, 'a', :space, ' ', :term, 'b b'],
+      '-a+ "h h"-'    => [:minus, '-', :term, 'a', :plus, '+', :space, ' ', :term, 'h h', :minus, '-'],
+      '-"\r :"zxcv'   => [:minus, '-', :term, "\r :", :term, 'zxcv'],
+    }
+
+    def test_lex
+      TESTS.each do |input, tokens|
+        assert_lex input, tokens
+      end
+    end
+
+    private
+
+    def assert_lex(input, stream)
+      result  = Lexer.new(input).lex
+      message = "input string: `#{input}` was not lex-ed correctly."
+
+      stream.each_slice(2) do |pair|
+        token = result.shift
+        assert token.type == pair[0], message
+        assert token.text == pair[1], message
+      end
+    end
+  end
+end

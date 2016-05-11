a7e110e2adf186f19d695931a6222d9ab4078fd5
Chakrit Wichian <service@chakrit.net>
2016-05-11 15:15:56 +0700

    Parallelize test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'truth_serum'
 
+require 'minitest/hell'
 require 'minitest/pride'
 require 'minitest/autorun'
+
+class Minitest::Test
+  parallelize_me!
+end

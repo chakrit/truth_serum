cbb152c29ed616e8bc68288b2c987f0f45ce0a18
Chakrit Wichian <service@chakrit.net>
2017-01-03 23:06:03 +0700

    Convert Lexer to use StateMachine class.

--- lib/truth_serum/lexer.rb
+++ lib/truth_serum/lexer.rb
@@ -1,128 +1,104 @@
 module TruthSerum
-  class Lexer
-    def initialize(input)
-      @input  = input
-    end
-
-    def reset
-      @index  = 0
-      @tokens = []
-      @buffer = ''
-    end
-
+  class Lexer < StateMachine
     def lex
-      reset
-      lex_line
-      @tokens
+      execute
     end
 
-    def lex_line
-      until eof?
-        case peek
-        when '"' then lex_quoted_word
-        when ':', '+', '-' then lex_symbol
-        when ' ' then lex_spaces
-        else
-          lex_word
-        end
+    state :start do
+      case peek
+      when nil then :end
+      when ' ' then :lex_space
+      when ':', '+', '-' then :lex_symbol
+      when '"' then :lex_quoted_term
+      else
+        :lex_term
       end
     end
 
-    def lex_symbol
-      case consume
+    state :lex_space do
+      case peek
+      when ' '
+        append(consume)
+        :lex_space
+      else
+        emit(:space)
+        :start
+      end
+    end
+
+    state :lex_symbol do
+      case append(consume)
       when ':' then emit(:colon)
       when '+' then emit(:plus)
       when '-' then emit(:minus)
       else
         raise 'not a symbol'
       end
+      :start
     end
 
-    def lex_spaces
-      consume while peek == ' '
-      emit(:space)
-    end
-
-    def lex_word
-      until eof?
-        case peek
-        when ' ', ':' then return emit(:term)
-        when '"' then discard
-        else
-          consume
-        end
-      end
-
-      emit(:term)
-    end
-
-    def lex_quoted_word
-      raise 'not in quoted word' unless discard == '"'
-
-      until eof?
-        case peek
-        when '\\' then lex_escaped_char
-        when '"'
-          discard
-          break
-        else
-          consume
-        end
-      end
-
-      emit(:term)
-    end
-
-    def lex_escaped_char
-      raise 'not in escaped char' unless discard == '\\'
-      return @buffer += '\\' if eof?
-
+    state :lex_term do
       case peek
-      when 'r'
-        discard
-        @buffer += "\r"
-      when 'n'
-        discard
-        @buffer += "\n"
-      else # '\\' '"' and everything else is treated literally
-        consume
+      when ' ', ':', nil
+        emit(:term)
+        :start
+      when '"'
+        consume # ignored
+        :lex_term
+      else
+        append(consume)
+        :lex_term
       end
+    end
+
+    state :lex_quoted_term do
+      raise 'not in quoted term' unless consume == '"'
+      :lex_inside_quoted_term
+    end
+
+    state :lex_inside_quoted_term do
+      case peek
+      when nil
+        emit(:term)
+        :start
+      when '\\'
+        :lex_quoted_char
+      when '"'
+        consume
+        emit(:term)
+        :start
+      else
+        append(consume)
+        :lex_inside_quoted_term
+      end
+    end
+
+    state :lex_quoted_char do
+      raise 'not in quoted char' unless consume == '\\'
+
+      case char = consume
+      when 'r'
+        append("\r")
+      when 'n'
+        append("\n")
+      else # '\\' '"' and everything else is passed verbatim
+        append(char)
+      end
+
+      :lex_inside_quoted_term
     end
 
     private
 
-    def eof?
-      @index >= @input.length
-    end
-
-    def chars_left
-      @input.length - @index
-    end
-
-    def peek
-      return nil if eof?
-      @input[@index]
-    end
-
-    def consume
-      raise 'consume beyond EOF' if eof?
-
-      @buffer += peek
-      discard
-    end
-
-    def discard
-      @index += 1
-      @input[@index - 1]
+    def append(ch)
+      @buffer ||= ''
+      @buffer += ch
+      ch
     end
 
     def emit(type)
-      return nil if @buffer == ''
-
-      token = Token.new(type, @buffer)
-      @tokens << token
-      @buffer = ''
-      token
+      super(Token.new(type, @buffer))
+      @buffer = nil
     end
   end
 end
